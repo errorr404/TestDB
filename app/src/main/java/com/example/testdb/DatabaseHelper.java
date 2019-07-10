e490fdaec05a349cc1a9a5ae4f8e829e13d29a4a
codebrahma <dkbishwash@gmail.com>
2019-07-10 19:51:00 +0530

    Created SQLite helper class and added a function for inserting data into database.

---
+++ app/src/main/java/com/example/testdb/DatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.testdb;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Simple_db";
+    public static final String TABLE_NAME = "Simple_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)" );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
+    }
+}
